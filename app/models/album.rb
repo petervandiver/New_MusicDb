42f8e92107f7fc37b7a3855ddb1b5c64b115602a
Peter Vandiver <pjvandiv@gmail.com>
2015-03-02 18:16:08 -0500

    add carrierwave and cover display

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,7 @@ class Album < ActiveRecord::Base
 	belongs_to :artist
 	has_many :songs
 
+
+	mount_uploader :cover, CoverUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302183248) do
+ActiveRecord::Schema.define(version: 20150302221342) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150302183248) do
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "cover"
   end
 
   create_table "artists", force: :cascade do |t|
